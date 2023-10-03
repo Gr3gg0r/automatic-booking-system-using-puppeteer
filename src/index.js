b020224c5004980e7c88627dd9bf7bf2035bff52
Shahfiq Shah <shahfiq.dev@gmail.com>
2023-10-03 18:34:05 +0800

    feat(scheduler): Add scheduler implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 require("dotenv").config();
-const puppeteer = require("puppeteer");
+const schedule = require("node-schedule");
 const config = require("./config");
-const dayjs = require("dayjs");
 const bookCourt = require("./services/bookCourt");
 const login = require("./services/login");
 
-const job = async () => {
+const bookJob = async () => {
   const cookies = await login(config.email, config.password);
   const radioValues = config.radioValue;
   for (const value of radioValues) {
     await bookCourt(cookies, value.id, value.value);
   }
+  return Promise.resolve();
 };
 
-job();
+const job = schedule.scheduleJob("03 00 * * 0,2,5", () => {
+  bookJob().catch((e) => {
+    console.log(e);
+  });
+});
 
+// Handle uncaught exceptions to prevent app termination
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught exception:", err);
+});
 
+// Handle unhandled promise rejections to prevent app termination
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled promise rejection:", reason);
+});
+
+console.log("Scheduler started.");
